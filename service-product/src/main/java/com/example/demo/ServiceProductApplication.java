02548a1be3b1e59b6b5c1bba34211e7cb52ee057
Jiahua Liu <jiahliu@thoughtworks.com>
2017-06-22 14:03:20 +0800

    refresh configuration online

--- service-product/src/main/java/com/example/demo/ServiceProductApplication.java
+++ service-product/src/main/java/com/example/demo/ServiceProductApplication.java
@@ -1,16 +1,27 @@
 package com.example.demo;
 
+import org.springframework.beans.factory.annotation.Value;
 import org.springframework.boot.SpringApplication;
 import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.context.config.annotation.RefreshScope;
 import org.springframework.web.bind.annotation.RequestMapping;
 import org.springframework.web.bind.annotation.RestController;
 
 import java.util.Arrays;
 import java.util.List;
 
+@RefreshScope
 @RestController
 @SpringBootApplication
 public class ServiceProductApplication {
+
+    @Value("${config.message: no message}")
+    private String message;
+
+    @RequestMapping("/hello")
+    String hello() {
+        return this.message;
+    }
 
     @RequestMapping("/categories")
     List<Category> categories() {
