0a7d0ff6e4ac015493a2ff7992abe57e7ff6675f
Jiahua Liu <jiahliu@thoughtworks.com>
2017-06-22 14:23:55 +0800

    Access service-product In BFF

---
+++ bff-test/src/main/java/com/example/demo/BffTestApplication.java
@@ -0,0 +1,70 @@
+package com.example.demo;
+
+import org.springframework.boot.SpringApplication;
+import org.springframework.boot.autoconfigure.SpringBootApplication;
+import org.springframework.cloud.client.discovery.EnableDiscoveryClient;
+import org.springframework.cloud.client.loadbalancer.LoadBalanced;
+import org.springframework.context.annotation.Bean;
+import org.springframework.core.ParameterizedTypeReference;
+import org.springframework.http.HttpMethod;
+import org.springframework.http.RequestEntity;
+import org.springframework.web.bind.annotation.GetMapping;
+import org.springframework.web.bind.annotation.RestController;
+import org.springframework.web.client.RestTemplate;
+
+import java.lang.reflect.Type;
+import java.net.URI;
+import java.net.URL;
+import java.util.List;
+import java.util.stream.Collectors;
+
+@RestController
+@EnableDiscoveryClient
+@SpringBootApplication
+public class BffTestApplication {
+
+    @LoadBalanced
+    @Bean
+    RestTemplate restTemplate() {
+        return new RestTemplate();
+    }
+
+    @GetMapping("/categoryNames")
+    public List<String> categoryNames() {
+        return restTemplate()
+                .exchange("http://service-product/categories",
+                        HttpMethod.GET,
+                        RequestEntity.EMPTY,
+                        new ParameterizedTypeReference<List<Category>>() {
+                        })
+                .getBody()
+                .stream()
+                .map(Category::getCategoryName)
+                .collect(Collectors.toList());
+    }
+
+    public static void main(String[] args) {
+        SpringApplication.run(BffTestApplication.class, args);
+    }
+}
+
+class Category {
+    private Integer id;
+    private String categoryName;
+
+    public Integer getId() {
+        return id;
+    }
+
+    public void setId(Integer id) {
+        this.id = id;
+    }
+
+    public String getCategoryName() {
+        return categoryName;
+    }
+
+    public void setCategoryName(String categoryName) {
+        this.categoryName = categoryName;
+    }
+}
